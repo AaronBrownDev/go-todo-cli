c0c0e5a34552a80b989b60b5917f00600cb895bf
AaronBrownDev <aaronb954@gmail.com>
2025-08-14 07:22:13 +0000

    feat: implement Priority enum

--- internal/todo/todo.go
+++ internal/todo/todo.go
@@ -2,11 +2,19 @@ package todo
 
 import "time"
 
+type Priority string
+
+const (
+	LOW_PRIORITY    Priority = "low"
+	MEDIUM_PRIORITY Priority = "medium"
+	HIGH_PRIORITY   Priority = "high"
+)
+
 type Todo struct {
 	ID          string    `json:"id"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
-	Priority    string    `json:"priority"` // TODO: Change data type to enum
+	Priority    Priority  `json:"priority"`
 	Category    string    `json:"category"`
 	DueDate     time.Time `json:"due_date"`
 	Completed   bool      `json:"completed"`
