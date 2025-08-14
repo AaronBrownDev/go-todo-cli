89cc51e6af839eea6a985c6a8ea49271de1efba4
AaronBrownDev <aaronb954@gmail.com>
2025-08-14 06:57:24 +0000

    feat: initialize todo list from data.json, creating file and directory if missing

--- internal/storage/json.go
+++ internal/storage/json.go
@@ -1,8 +1,47 @@
 package storage
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/AaronBrownDev/go-todo-cli/internal/todo"
 )
+
+var todoList []todo.Todo
+var jsonPath string
+
+func init() {
+	// Grabs home directory path. If fails, prints the error then exits program with code 1.
+	homePath, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Adds the designated json file path together with OS specific separators
+	jsonPath = filepath.Join(homePath, ".todo", "data.json")
+
+	// If the file does not exist, creates the path and data.json file
+	if _, err = os.ReadFile(jsonPath); os.IsNotExist(err) {
+		os.Mkdir(filepath.Join(homePath, ".todo"), 0700)
+		os.WriteFile(jsonPath, []byte("[]\n"), 0600)
+	}
+
+	// Reads the json file and returns the content as a slice of bytes. If fails, prints the error then exits program with code 1.
+	content, err := os.ReadFile(jsonPath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Unmarshals the slice of bytes into a slice of todo.Todo. If fails, prints the error then exits program with code 1.
+	if err = json.Unmarshal(content, &todoList); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
 
 type jsonRepository struct {
 	// TODO
